d3df25d319c8102414c4d85ddf8afc39bfafa7fb
makmoul soufiane <makmoulsoufiane2@gmail.com>
2026-05-08 14:06:21 +0100

    add spiner loading and note message when empy note in home pafe

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,6 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
   console.log(notes);
 
-
   useEffect(() => {
     const fetchNotes = async () => {
       try {
@@ -35,25 +34,45 @@ const HomePage = () => {
   }, []);
 
   return (
-  <div className="min-h-screen">
-    <NavBar />
+    <div className="min-h-screen">
+      <NavBar />
 
-    {isRateLimited && <RateLimited />}
+      {isRateLimited && <RateLimited />}
 
-    {/* NOTES */}
-    {!loading && (
-      <div className="max-w-6xl mx-auto px-6 py-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {notes.map((note) => (
-            <div key={note._id} className="h-full">
-              <NoteCard note={note}  setNotes={setNotes} />
-            </div>
-          ))}
+      {loading && (
+        <div className="flex flex-col items-center justify-center py-10 gap-3">
+          <span className="loading loading-spinner loading-lg text-primary"></span>
+          <p className="text-sm text-base-content/70">Loading notes...</p>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+
+      {notes.length === 0 && (
+        <div className="flex flex-col items-center justify-center py-20">
+          <h2 className="text-4xl font-extrabold text-green-700 mb-4">
+            No Notes Yet
+          </h2>
+
+          <p className="text-lg text-gray-500 text-center max-w-md">
+            You don’t have any notes right now. Start by creating your first
+            note 🚀
+          </p>
+        </div>
+      )}
+
+      {/* NOTES */}
+      {!loading && (
+        <div className="max-w-6xl mx-auto px-6 py-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {notes.map((note) => (
+              <div key={note._id} className="h-full">
+                <NoteCard note={note} setNotes={setNotes} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
